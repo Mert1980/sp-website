ae9e54ee2d13701a5bf59b101a3c7e7963413da4
ilhan_k <ilhan.k@sparkcentral.com>
2024-10-05 00:48:54 +0200

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "SPX",
+  url: "https://spx.network",
+  description:
+    "SPX is a digital transformation company that helps businesses grow and innovate.",
+  author: {
+    name: "SPX",
+    email: "info@smartyprox.com",
+  },
+  links: {
+    twitter: "https://twitter.com/spx_network",
+    linkedin: "https://www.linkedin.com/company/spx-network",
+    github: "https://github.com/ncaliber",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
